dcad00dec13af32a4ff0863252e830d310b18eb5
Galang Sakti G <wadepakmen97@gmail.com>
2024-05-13 11:38:43 +0700

    class mahasiswa public int nim string nama

---
+++ Constructor1/Constructor1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Mahasiswa
+{
+public:
+	int nim;
+	string nama;
+
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "Galang Sakti";
+	};
+	Mahasiswa(int iNIM) {nim = iNIM;}
+	Mahasiswa(string iNama); {nama = iNama;}
+	Mahasiswa(int iNIM, string iNama);
+
+	void cetak();
+};
+
+
